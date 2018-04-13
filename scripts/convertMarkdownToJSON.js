e5873be2f286e4389fbc06a27fffe79284cfab51
Kinnan Kwok <kkwoker@gmail.com>
2018-04-12 18:04:49 -0700

    use marked to get pre tags on code snippets

--- scripts/convertMarkdownToJSON.js
+++ scripts/convertMarkdownToJSON.js
@@ -1,15 +1,18 @@
+const path = require('path');
 const paths = require('../config/paths');
 const fs = require('fs');
 const fm = require('front-matter');
-const markdown = require("markdown").markdown;
+const marked = require('marked');
 
 fs.readdir(paths.postsDir, (err, files) => {
   const postsList = [];
-  files.forEach(file => {
+  const mdFiles = files.filter(file => path.extname(file) === '.md');
+  mdFiles.forEach(file => {
+    console.log('Converting file', file)
     const content = fs.readFileSync(paths.postsDir + '/' + file, 'utf-8');
 
     const metadata = fm(content);
-    const html = markdown.toHTML(metadata.body);
+    const html = marked(metadata.body);
 
     const post = Object.assign(
       metadata.attributes,
