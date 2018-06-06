1c5177a3591a7549e46454ab227c2eec1ecaaedb
Kinnan Kwok <kkwoker@gmail.com>
2018-06-06 14:28:30 -0700

    article cards

--- src/pages/Articles/Styles.js
+++ src/pages/Articles/Styles.js
@@ -29,6 +29,30 @@ export const BackgroundImage = styled.div`
 `;
 
 export const ArticleList = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap
+  .card-section {
+    margin: 20px 10px;
+  }
   div {
   }
 `;
+
+export const ArticleCard = styled.div`
+  a {
+    text-decoration: none;
+  }
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2), 0 4px 20px 0 rgba(0,0,0,0.19);
+  width: 300px;
+  img {
+    width: 100%;
+  }
+
+  .container {
+    padding: 0.01em 16px;
+  }
+`
--- src/pages/Articles/index.jsx
+++ src/pages/Articles/index.jsx
@@ -1,17 +1,80 @@
 import React from 'react';
-import { BackgroundImage, ArticleList } from './Styles';
+import { BackgroundImage, ArticleList, ArticleCard } from './Styles';
 
 class Articles extends React.Component {
   render() {
     return (
       <div>
         <BackgroundImage />
-            <h1>Articles that have englightend me</h1>
+        <h1>Articles that have englightend me</h1>
+
         <ArticleList>
-          <a href="https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/">
-            The care and feeding of software engineers (or, why engineers are grumpy)
-          </a>
-          (Show a link preview)
+
+          <div className='card-section'>
+            <ArticleCard>
+              <a href='https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/'>
+                <img src='https://www.w3schools.com/w3css/img_5terre.jpg' />
+                <div className='container'>
+                  <h4>Title</h4>
+                  <p>
+                    The care and feeding of software engineers (or, why engineers are grumpy)
+                  </p>
+                </div>
+              </a>
+            </ArticleCard>
+          </div>
+          <div className='card-section'>
+            <ArticleCard>
+              <a href='https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/'>
+                <img src='https://www.w3schools.com/w3css/img_5terre.jpg' />
+                <div className='container'>
+                  <h4>Title</h4>
+                  <p>
+                    The care and feeding of software engineers (or, why engineers are grumpy)
+                  </p>
+                </div>
+              </a>
+            </ArticleCard>
+          </div>
+          <div className='card-section'>
+            <ArticleCard>
+              <a href='https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/'>
+                <img src='https://www.w3schools.com/w3css/img_5terre.jpg' />
+                <div className='container'>
+                  <h4>Title</h4>
+                  <p>
+                    The care and feeding of software engineers (or, why engineers are grumpy)
+                  </p>
+                </div>
+              </a>
+            </ArticleCard>
+          </div>
+          <div className='card-section'>
+            <ArticleCard>
+              <a href='https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/'>
+                <img src='https://www.w3schools.com/w3css/img_5terre.jpg' />
+                <div className='container'>
+                  <h4>Title</h4>
+                  <p>
+                    The care and feeding of software engineers (or, why engineers are grumpy)
+                  </p>
+                </div>
+              </a>
+            </ArticleCard>
+          </div>
+          <div className='card-section'>
+            <ArticleCard>
+              <a href='https://www.nczonline.net/blog/2012/06/12/the-care-and-feeding-of-software-engineers-or-why-engineers-are-grumpy/'>
+                <img src='https://www.w3schools.com/w3css/img_5terre.jpg' />
+                <div className='container'>
+                  <h4>Title</h4>
+                  <p>
+                    The care and feeding of software engineers (or, why engineers are grumpy)
+                  </p>
+                </div>
+              </a>
+            </ArticleCard>
+          </div>
 
         </ArticleList>
 
